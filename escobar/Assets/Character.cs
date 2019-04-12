58f9c49f7ae68ba9705783085088806347a31a2a
DESKTOP-FI1D7HI\pontura <pontura@gmail.com>
2019-04-12 14:17:12 -0700

    audios

---
+++ escobar/Assets/Character.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public AudioSpectrum audioSpectrum;
+    float audioValue;
+
+    public GameObject mouth;
+    public GameObject ClosedMouth;
+
+    Vector3 originalPosition;
+    public float openingFactor;
+    public float scaleFactor;
+    Vector3 originalsize;
+
+    void Start()
+    {
+        originalPosition = mouth.transform.localPosition;
+        originalsize = mouth.transform.localScale;
+    }
+
+    void Update()
+    {
+        audioValue = audioSpectrum.result;
+        if (audioValue < 0.01f)
+        {
+            CloseMouth();
+            return;
+        } else
+        {
+            ClosedMouth.SetActive(false);
+            mouth.SetActive(true);
+            Vector3 pos = mouth.transform.localPosition;
+            pos.y = originalPosition.y += audioValue * openingFactor;
+            mouth.transform.localPosition = pos; 
+        }
+
+    }
+    void CloseMouth()
+    {
+        ClosedMouth.SetActive(true);
+        mouth.SetActive(false);
+    }
+}
