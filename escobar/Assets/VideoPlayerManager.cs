d1d9e7fd5dd023a460f3acef6b91f0a07745bb70
jpamato <juanpamato@gmail.com>
2019-04-25 10:57:45 -0300

    http y loading

--- escobar/Assets/VideoPlayerManager.cs
+++ escobar/Assets/VideoPlayerManager.cs
@@ -5,20 +5,24 @@ using UnityEngine.Video;
 public class VideoPlayerManager : MonoBehaviour
 {
     public VideoPlayer videoPlayer;
+    public GameObject loading;
 
     void Start()
     {
         Events.OnNewQuestion += OnNewQuestion;
         Events.OnPreLoadVideo += OnPreLoadVideo;
         videoPlayer.loopPointReached += EndReached;
+        videoPlayer.prepareCompleted += Prepared;
+
     }
     void OnPreLoadVideo(string file)
     {
         print("Preloading... " + file);
-        videoPlayer.url = file;
+        videoPlayer.url = file.Replace("https","http");
     }
     void OnNewQuestion(JWPlayerData.PlaylistData data)
     {
+        loading.SetActive(true);
         print("OnNewQuestion");
         videoPlayer.Play();
     }
@@ -28,4 +32,7 @@ public class VideoPlayerManager : MonoBehaviour
         Events.OnShowTrivia();
     }
 
+    void Prepared(UnityEngine.Video.VideoPlayer vp) {
+        loading.SetActive(false);
+    }
 }
