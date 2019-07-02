026bfab3a7578fe7f3a4e16e7eb78bd1152f07e3
Agustin Pontura <pontura@gmail.com>
2019-07-02 19:23:46 -0300

    stats de usuarios

---
+++ escobar/Assets/ResultsAdmin.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResultsAdmin : MainScreen
+{
+    public AdminButton adminButton;
+    public Transform container;
+    public GameObject isEmptyPanel;
+    public Text otherField;
+
+    public states state;
+    public enum states
+    {
+        ONLY_ACTIVE,
+        ALL
+    }
+
+    public List<AdminButton> all;
+
+    public override void OnEnabled()
+    {
+        Data.Instance.capitulosData.OnRefreshCapitulos();
+        state = states.ONLY_ACTIVE;
+        SetOtherButton();
+        Load();
+    }
+    void Load()
+    {
+        if (Data.Instance.capitulosData.capitulos.Count == 0)
+        {
+            Invoke("Load", 0.2f);
+        }
+        else
+        {
+            DrawData();
+        }
+    }
+    void DrawData()
+    {
+        CancelInvoke();
+        Data.Instance.capitulosData.activeCapitulo = null;
+        int id = 0;
+        all.Clear();
+        Utils.RemoveAllChildsIn(container);
+        foreach (CapitulosData.Capitulo c in Data.Instance.capitulosData.capitulos)
+        {
+            if (
+                (state == states.ONLY_ACTIVE && !Data.Instance.dateData.IsFromThePast(c.date))
+                ||
+                state == states.ALL
+                )
+            {
+                AdminButton button = Instantiate(adminButton);
+                button.transform.SetParent(container);
+                button.transform.localScale = Vector3.one;
+                button.Init(id, c.date + " " + c.time + "hs", OnClicked);
+                all.Add(button);
+            }
+            id++;
+        }
+        if (all.Count == 0)
+            isEmptyPanel.SetActive(true);
+        else
+            isEmptyPanel.SetActive(false);
+    }
+    void OnClicked(AdminButton button)
+    {
+        print("CapitulosAdmin clicked" + button.id);
+        Data.Instance.capitulosData.activeCapitulo = Data.Instance.capitulosData.capitulos[button.id];
+        Data.Instance.triviaData.LoadPlaylist(Data.Instance.capitulosData.activeCapitulo.playlistID, GotoEdit, true);
+    }
+    public void AddNew()
+    {
+        GotoEdit();
+    }
+    void GotoEdit()
+    {
+        UI.Instance.screensManager.LoadScreen(4, true);
+    }
+    public void OthersClicked()
+    {
+        if (state == states.ONLY_ACTIVE)
+        {
+            state = states.ALL;
+        }
+        else
+        {
+            state = states.ONLY_ACTIVE;
+        }
+        SetOtherButton();
+        DrawData();
+    }
+    void SetOtherButton()
+    {
+        if (state == states.ONLY_ACTIVE)
+            otherField.text = "VER TODOS";
+        else
+            otherField.text = "SOLO ACTIVOS";
+    }
+}
---
+++ escobar/Assets/ResultsData.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase.Database;
+using System;
+
+public class ResultsData : MonoBehaviour
+{
+
+    public List<Participante> participantes;
+
+    [Serializable]
+    public class Participante
+    {
+        public string uid;
+        public List<Results> respuestas;
+        public float totalTimeCorrect;
+        public int totalCorrect;
+    }
+    [Serializable]
+    public class Results
+    {
+        public int respuesta;
+        public float timer;
+    }
+    public CapitulosData.Capitulo activeCapitulo;
+
+    void Start()
+    {
+        LoopToLoadCapitulos();
+    }
+    void LoopToLoadCapitulos()
+    {
+        if (Data.Instance.capitulosData.capitulos.Count == 0)
+        {
+            Invoke("LoopToLoadCapitulos", 1);
+        }
+        else if (UI.Instance.screensManager.isAdmin)
+            OnRefreshParticipantes();
+    }   
+    public void OnRefreshParticipantes()
+    {
+        participantes.Clear();
+        LoadData("-LinLrfHJmmZVpD_ITq1");
+    }
+    void LoadData(string uid)
+    {
+        string url = "capitulos_participantes/" + uid + "/participantes";
+        Events.OnGetServerData(url, OnReady);
+        print("LoadData url: " + url);
+    }
+    void OnReady(DataSnapshot snapshot)
+    {
+        print("OnReady snapshot: " + snapshot);
+        participantes.Clear();
+        foreach (DataSnapshot data in snapshot.Children)
+        {
+            Participante tData = new Participante();
+            IDictionary dataDictiontary = (IDictionary)data.Value;
+            tData.uid = data.Key;
+            tData.respuestas = new List<Results>();
+            
+            List<object> all = (List<object>)dataDictiontary["respuestas"];
+
+            int totalCorrect = 0;
+            float totalTimeCorrect = 0;
+            foreach(object o in all)
+            {
+                Results result = new Results();
+                IDictionary d = (IDictionary)o;
+
+                string respuesta = d["respuesta"].ToString();
+                string timer = d["timer"].ToString();
+
+                result.respuesta = int.Parse(respuesta);
+                result.timer = float.Parse(timer);
+                tData.respuestas.Add(result);
+
+                if (result.respuesta == 0)
+                {
+                    totalTimeCorrect += result.timer;
+                    totalCorrect++;
+                }
+                    
+            }
+            tData.totalCorrect = totalCorrect;
+            tData.totalTimeCorrect = totalTimeCorrect;
+            participantes.Add(tData);
+        }
+    }
+   
+}
